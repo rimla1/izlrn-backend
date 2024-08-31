884398f4d119e4bfb59471d56b8b1305a5c3015d
Almir Muminovic <projectalmir@gmail.com>
2024-08-31 18:30:38 +0200

    Enabling CORS for all adresses

--- src/main.ts
+++ src/main.ts
@@ -7,11 +7,11 @@ dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  // app.enableCors({
-  //   origin: 'http://localhost:5173',
-  //   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-  //   allowedHeaders: 'Content-Type, Authorization',
-  // });
+  app.enableCors({
+    allowedHeaders: '*',
+    origin: '*',
+    credentials: true,
+  });
 
   const PORT = process.env.PORT || 3000
   console.log("Trying to run on PORT: ", PORT)
