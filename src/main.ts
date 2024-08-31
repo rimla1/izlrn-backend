da060b68149efbecbfe01d5808c4873f228ae2e2
Almir Muminovic <projectalmir@gmail.com>
2024-08-31 18:02:46 +0200

    comment cors

--- src/main.ts
+++ src/main.ts
@@ -7,11 +7,11 @@ dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: 'http://localhost:5173',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-    allowedHeaders: 'Content-Type, Authorization',
-  });
+  // app.enableCors({
+  //   origin: 'http://localhost:5173',
+  //   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+  //   allowedHeaders: 'Content-Type, Authorization',
+  // });
 
   const PORT = process.env.PORT || 3000
   console.log("Running on PORT: ", PORT)
