ae1b4e64579ff58349828d098acbc5f583010702
Almir Muminovic <projectalmir@gmail.com>
2024-10-29 15:11:52 +0100

    Allowing origin to everyone to test insomnia

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'https://izlrn.com',
+    origin: '*',
     methods: 'GET,POST,PUT,DELETE,OPTIONS',
     credentials: true,
   });
