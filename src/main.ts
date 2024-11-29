d5f233b087f1232e24cfc412ac0c3bee44885dba
Almir Muminovic <projectalmir@gmail.com>
2024-11-29 18:23:39 +0100

    Server running on port 443

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as dotenv from 'dotenv';
+import * as fs from 'fs';
 
-dotenv.config(); 
+dotenv.config();
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  let httpsOptions;
+  try {
+    httpsOptions = {
+      key: fs.readFileSync('/etc/letsencrypt/live/api.izlrn.com/privkey.pem'),
+      cert: fs.readFileSync('/etc/letsencrypt/live/api.izlrn.com/cert.pem'),
+    };
+  } catch (error) {
+    console.error('Error reading HTTPS options: ', error);
+    process.exit(1);
+  }
+
+  const app = await NestFactory.create(AppModule, { httpsOptions });
 
   app.enableCors({
     origin: '*',
@@ -13,10 +25,11 @@ async function bootstrap() {
     credentials: true,
   });
 
-  const PORT = process.env.PORT || 3000
-  
+  const PORT = process.env.PORT || 443;
+
   await app.listen(PORT, '0.0.0.0', () => {
-    console.log("Server is up and running on port: ", PORT)
+    console.log('Server is up and running on port: ', PORT);
   });
 }
+
 bootstrap();
